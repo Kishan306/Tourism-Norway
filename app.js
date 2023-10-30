b4700e581927c8972c124a5b329a9da30e6f205d
Kishan306 <kp1140753@gmail.com>
2023-10-30 17:16:40 +0530

    placed all images in one folder

--- app.js
+++ app.js
@@ -7,3 +7,38 @@ function myFunction() {
     document.getElementById("back-to-top-id").className = "back-to-top";
   }
 }
+
+const correctAnswers = ['A', 'C', 'B', 'A'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result'); 
+
+form.addEventListener('submit', e => {
+  
+  e.preventDefault();
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+  //check answers
+  userAnswers.forEach((answer, index) => {
+      if(answer === correctAnswers[index]){
+          score += 25;
+      }
+  });
+
+  //show result on page
+  
+  result.querySelector('span').textContent = `${score}%`;
+  result.classList.remove('displayResult');
+
+  let output = 0;
+  const timer = setInterval(() => {
+      result.querySelector('.result').textContent = `${output}%`;
+      if(output === score){
+          clearInterval(timer);
+      }else{
+          output++;
+      }
+  }, 10);
+  scrollTo(0,document.body.scrollHeight);
+  
+});
